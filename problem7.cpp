4bc1f83d9db949965e914518adcada4cce4d97fe
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-31 17:20:58 +0530

    Create problem7.cpp

---
+++ problem7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+	int a,b,c;
+	cout <<"Enter two angles of a triangle";
+	cin>>a>>b;
+	c=180-(a+b);
+	cout<<"the third angle ="<<c;
+    return 0;
+}

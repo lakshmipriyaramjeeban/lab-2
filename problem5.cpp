534548534cf1349b6ea0722e9fb6d8c31af51397
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-31 17:01:26 +0530

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    int x=1,y=1,z;
+    cout<<"Enter x and y  to find x^y";
+    cin>>x>>y;
+    z=pow(x,y);
+    cout<< z;
+
+	return 0;
+}

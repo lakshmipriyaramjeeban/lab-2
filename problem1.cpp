77377c868c2d297725f4b0d60a36ef452219e260
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-08-31 15:30:55 +0530

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float c,m,k;
+	cout<<"enter the length in centimeter";
+	cin>>c;
+	m=c/100;
+	cout<<"the centimeter converted into meter"<<m;
+	k=c/100000;
+	cout<<"the centimeter converted into meter"<<k;
+
+	return 0;
+}

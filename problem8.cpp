1f50c840442a239e74f2d06ecd5f5827c8d1c122
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-05 20:28:45 +0530

    Create problem8.cpp

---
+++ problem8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int b,h,a;
+	cout<<"Enter height:";
+	cin>>h;
+	cout<<"Enter base:";
+	cin>>b;
+	a=(b*h)/2;
+	cout<<"Area of right angle is:"<<a;
+	
+	
+	return 0;
+}

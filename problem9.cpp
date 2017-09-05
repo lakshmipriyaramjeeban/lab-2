860e857385fb814b6d4850a2617b6ec46c59eb7b
lakshmipriyaramjeeban <31353769+lakshmipriyaramjeeban@users.noreply.github.com>
2017-09-05 20:57:58 +0530

    Create problem9.cpp

---
+++ problem9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main() 
+{
+	float r,a;
+	cout<<"Enter side:";
+	cin>>r;
+	a=sqrt((3)*r*r)/4;
+	cout<<"Area of triangle is:"<<a;
+	return 0;
+}
